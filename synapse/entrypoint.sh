27303d363c184963ce94dd2ff6a872dbc32f87b9
Huotsty <penghout78@gmail.com>
2026-02-05 20:21:26 +0700

    update the # to | in the script

--- synapse/entrypoint.sh
+++ synapse/entrypoint.sh
@@ -15,15 +15,15 @@ SYNAPSE_DB_PORT="${SYNAPSE_DB_PORT:-5432}"
 mkdir -p /data /data/media_store
 
 sed \
-  -e "s#{{SYNAPSE_SERVER_NAME}}#${SYNAPSE_SERVER_NAME}#g" \
-  -e "s#{{SYNAPSE_PUBLIC_BASEURL}}#${SYNAPSE_PUBLIC_BASEURL}#g" \
-  -e "s#{{SYNAPSE_DB_USER}}#${SYNAPSE_DB_USER}#g" \
-  -e "s#{{SYNAPSE_DB_PASSWORD}}#${SYNAPSE_DB_PASSWORD}#g" \
-  -e "s#{{SYNAPSE_DB_HOST}}#${SYNAPSE_DB_HOST}#g" \
-  -e "s#{{SYNAPSE_DB_PORT}}#${SYNAPSE_DB_PORT}#g" \
-  -e "s#{{SYNAPSE_DB_NAME}}#${SYNAPSE_DB_NAME}#g" \
-  -e "s#{{SYNAPSE_MACAROON_SECRET_KEY}}#${SYNAPSE_MACAROON_SECRET_KEY}#g" \
-  -e "s#{{SYNAPSE_FORM_SECRET}}#${SYNAPSE_FORM_SECRET}#g" \
+  -e "s|{{SYNAPSE_SERVER_NAME}}|${SYNAPSE_SERVER_NAME}|g" \
+  -e "s|{{SYNAPSE_PUBLIC_BASEURL}}|${SYNAPSE_PUBLIC_BASEURL}|g" \
+  -e "s|{{SYNAPSE_DB_USER}}|${SYNAPSE_DB_USER}|g" \
+  -e "s|{{SYNAPSE_DB_PASSWORD}}|${SYNAPSE_DB_PASSWORD}|g" \
+  -e "s|{{SYNAPSE_DB_HOST}}|${SYNAPSE_DB_HOST}|g" \
+  -e "s|{{SYNAPSE_DB_PORT}}|${SYNAPSE_DB_PORT}|g" \
+  -e "s|{{SYNAPSE_DB_NAME}}|${SYNAPSE_DB_NAME}|g" \
+  -e "s|{{SYNAPSE_MACAROON_SECRET_KEY}}|${SYNAPSE_MACAROON_SECRET_KEY}|g" \
+  -e "s|{{SYNAPSE_FORM_SECRET}}|${SYNAPSE_FORM_SECRET}|g" \
   /data/homeserver.template.yaml > /data/homeserver.yaml
 
 if [[ ! -f /data/synapse.log.config ]]; then
